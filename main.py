b26b27d04e150c71a452bf331b8a885d4defb482
Julian Pinzer <jupinzer@microsoft.com>
2021-10-12 13:57:21 -0400

    Switch APIs to the tmdb api.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import json
-import requests
 from dotenv import dotenv_values
-from imdb_api.imdb_api import Imdb_Api
+import tmdbsimple as tmdb
 
-config = dotenv_values(".env")
-api_key = config["API_KEY"]
+tmdb.API_KEY = dotenv_values(".env")["API_KEY"]
 
-HER_ID = 'tt1798709'
-
-api_obj = Imdb_Api(api_key)
-
-movie_content = api_obj.Title(HER_ID)
-print(json.dumps(movie_content.get(), indent = 2))
+movie = tmdb.Movies(603)
+response = movie.info()
+print(json.dumps(response, indent=2))
