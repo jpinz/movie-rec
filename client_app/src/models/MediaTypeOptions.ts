1799f837e9a823770d677a6870a7c381b14ca194
Julian Pinzer <jupinzer@microsoft.com>
2021-10-12 18:43:09 -0400

    The beginnings of the front-end. Has the first page just about done, select media type and region. Have to fix the region input to auto complete.

---
+++ client_app/src/models/MediaTypeOptions.ts
@@ -0,0 +1,6 @@
+enum MediaTypeOptions {
+    Movie,
+    TV,
+}
+
+export default MediaTypeOptions;
---
+++ client_app/src/pages/Page1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MediaTypes from '../components/MediaTypes';
+import RegionComponent from '../components/Region';
+
+function Page1() {
+  return (
+    <div className="Page1">
+        <MediaTypes />
+        <RegionComponent />
+    </div>
+  );
+}
+
+export default Page1;
