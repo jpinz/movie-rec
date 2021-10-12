e015b95d43cf89af0138f72c4b79a7094239af06
Julian <julian.pinzer@gmail.com>
2021-10-12 16:29:38 +0000

    Create the beginning of the API library itself. Add a method to get the content for a movie title.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import requests
 import json
+import requests
 from dotenv import dotenv_values
+from api.title import title
+
 config = dotenv_values(".env")
 api_key = config["API_KEY"]
 
 her_id = 'tt1798709'
-search_api_url = 'https://imdb-api.com/en/API/Title/'
 
-response = requests.get(search_api_url + api_key + '/' + her_id, timeout=5)
-
-movie_content = json.loads(response.content)
+movie_content = title(api_key, her_id)
 print(json.dumps(movie_content, indent = 2))
