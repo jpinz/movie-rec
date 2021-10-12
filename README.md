6d8bb8d3d37550f94ac7eaa0c4c2e615f81fb2c8
cormacpayne <corm@microsoft.com>
2021-10-12 13:25:06 -0700

    - Update README.md to acknowledge TMDb and JustWatch - Add endpoints to get genres for movies and TV shows

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # movie-rec
 Our attempt at creating a system to recommend a similar movie given a user's preferences.
+
+This project leverages [TMDb's](https://www.themoviedb.org) API to grab metadata about movies and TV shows (full details
+on TMDb's API can be found [here](https://developers.themoviedb.org/3/getting-started/introduction)), as well as
+[JustWatch](https://www.justwatch.com/) (partnered with TMDb) to determine the available providers for movies and TV
+shows per country.
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask import Flask, session, redirect, url_for, escape, request
 import constants
 
 base_api_endpoint = f'/api/v{constants.API_VERSION}'
+tmdb_base_api_endpoint = 'https://api.themoviedb.org/3'
+tmdb_api_key = dotenv_values(".env")["API_KEY"]
 app = Flask(__name__)
 
 
@@ -17,6 +19,16 @@ def get_country(ip_address):
     except Exception as e:
         return "Unknown"
 
+def get_genres(type):
+    if type == "movies":
+        tmdb_genre_endpoint = f'{tmdb_base_api_endpoint}/genre/movie/list?api_key={tmdb_api_key}'
+    elif type == "tv":
+        tmdb_genre_endpoint = f'{tmdb_base_api_endpoint}/genre/tv/list?api_key={tmdb_api_key}'
+    else:
+        raise NotImplementedError
+    response = requests.get(tmdb_genre_endpoint)
+    genres = response.json()['genres']
+    return genres
 
 @app.route('/welcome/', methods=['GET'])
 def welcome():
@@ -45,6 +57,24 @@ def api_region():
     country = get_country(request.remote_addr)
     session['countryCode'] = country
     return country
+
+# A route to get movie genres from TMDb
+@app.route(f'{base_api_endpoint}/movie/genres', methods=['GET'])
+def api_movie_genres():
+    if session.get('movie_genres'):
+        return session['movie_genres']
+    genres = get_genres("movie")
+    session['movie_genres'] = genres
+    return genres
+
+# A route to get TV genres from TMDb
+@app.route(f'{base_api_endpoint}/tv/genres', methods=['GET'])
+def api_tv_genres():
+    if session.get('tv_genres'):
+        return session['tv_genres']
+    genres = get_genres("tv")
+    session['tv_genres'] = genres
+    return genres
 
 if __name__ == '__main__':
     app.secret_key = 'floridasux'
